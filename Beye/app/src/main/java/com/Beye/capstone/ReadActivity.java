1f2bdc29104d5c6266a5f0820e5c145158ef6be0
dispe1 <lkjim0757@naver.com>
2019-03-28 18:10:01 +0900

    main ui

---
+++ Beye/app/src/main/java/com/Beye/capstone/ReadActivity.java
@@ -0,0 +1,15 @@
+package com.Beye.capstone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read);
+
+
+    }
+}
