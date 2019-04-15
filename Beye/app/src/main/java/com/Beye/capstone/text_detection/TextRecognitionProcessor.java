a06604b94d9403d22b22e34f2eb1ef49271191c1
Unknown <xogus1107@naver.com>
2019-04-15 15:40:29 +0900

    modify busnumber ocr

--- Beye/app/src/main/java/com/Beye/capstone/text_detection/TextRecognitionProcessor.java
+++ Beye/app/src/main/java/com/Beye/capstone/text_detection/TextRecognitionProcessor.java
@@ -98,23 +98,37 @@ public class TextRecognitionProcessor {
 	protected void onSuccess(@NonNull FirebaseVisionText results, @NonNull FrameMetadata frameMetadata, @NonNull GraphicOverlay graphicOverlay) {
 
         BusNumActivity bus = new BusNumActivity();
-
+		boolean hancheck=false;
+		if(bus.getBusNum().matches(".*[가-힣]")){
+			hancheck=true;
+		}
         graphicOverlay.clear();
         String resultText = results.getText();
         for (FirebaseVisionText.TextBlock block: results.getTextBlocks()) {
             for (FirebaseVisionText.Line line: block.getLines()) {
                 for (FirebaseVisionText.Element element: line.getElements()) {
                     String elementText = element.getText();
-                    //System.out.println(elementText);
-                    if(elementText.equals(bus.getBusNum())){
-                        System.out.println("i Fuckin found it");
-                        BusNumActivity.tts.speak("전방에"+bus.getBusNum()+"번 버스입니다.",TextToSpeech.QUEUE_FLUSH,null);
-                        try {
-                            Thread.sleep(5000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
+
+					if(hancheck==true){
+						if(bus.getBusNum().contains(elementText)){
+							BusNumActivity.tts.speak("전방에" + bus.getBusNum() + "번 버스입니다.", TextToSpeech.QUEUE_FLUSH, null);
+							try {
+								Thread.sleep(3000);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						}
+					}
+					else if(hancheck==false) {
+						if (elementText.contains(bus.getBusNum())) {
+							BusNumActivity.tts.speak("전방에" + bus.getBusNum() + "번 버스입니다.", TextToSpeech.QUEUE_FLUSH, null);
+							try {
+								Thread.sleep(3000);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						}
+					}
                 }
             }
         }
