25ef1581b695475594bf5defc2dd6d9711d3f8d6
dispe1 <lkjim0757@naver.com>
2019-05-03 20:32:14 +0900

    add bus arrival time

--- Beye/app/src/main/java/com/Beye/capstone/text_detection/TextRecognitionProcessor.java
+++ Beye/app/src/main/java/com/Beye/capstone/text_detection/TextRecognitionProcessor.java
@@ -104,6 +104,7 @@ public class TextRecognitionProcessor {
 		if(check==5){
 			hancheck=true;
 		}
+<<<<<<< Updated upstream
 		graphicOverlay.clear();
 		String resultText = results.getText();
 		for (FirebaseVisionText.TextBlock block: results.getTextBlocks()) {
@@ -125,6 +126,22 @@ public class TextRecognitionProcessor {
 								} catch (InterruptedException e) {
 									e.printStackTrace();
 								}
+=======
+        graphicOverlay.clear();
+        String resultText = results.getText();
+        for (FirebaseVisionText.TextBlock block: results.getTextBlocks()) {
+            for (FirebaseVisionText.Line line: block.getLines()) {
+                for (FirebaseVisionText.Element element: line.getElements()) {
+                    String elementText = element.getText();
+
+					if(hancheck==true){
+						if(bus.getBusNum().contains(elementText)){
+							BusNumActivity.tts.speak("전방에" + bus.getBusNum() + "번 버스입니다.", TextToSpeech.QUEUE_FLUSH, null);
+							try {
+								Thread.sleep(3000);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+>>>>>>> Stashed changes
 							}
 						} else if (hancheck == false) {
 							if (elementText.contains(bus.getBusNum())) {
@@ -138,9 +155,24 @@ public class TextRecognitionProcessor {
 							}
 						}
 					}
+<<<<<<< Updated upstream
 				}
 			}
 		}
+=======
+                    //System.out.println(elementText);
+                    if(elementText.equals(bus.getBusNum())){
+                        BusNumActivity.tts.speak("전방에"+bus.getBusNum()+"번 버스입니다.",TextToSpeech.QUEUE_FLUSH,null);
+                        try {
+                            Thread.sleep(5000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+>>>>>>> Stashed changes
 
 		List<FirebaseVisionText.TextBlock> blocks = results.getTextBlocks();
 		for (int i = 0; i < blocks.size(); i++) {
